f0d3423cb5e90f5eb989ac3ec19598787c4f9cb8
Hidenori Miyoshi <miyohide@gmail.com>
2018-04-28 23:51:11 +0900

    First implement. Card class.

---
+++ lib/card.rb
@@ -0,0 +1,24 @@
+class Card
+  SUITS = %i(heart diamond club spade)
+  NUMBERS = %W(A 1 2 3 4 5 6 7 8 9 10 J Q K)
+
+  def self.generate_stock
+    SUITS.product(NUMBERS).map { |suit, number| self.new(suit, number) }.shuffle
+  end
+
+  def initialize(suit, number)
+    @suit = suit
+    @number = number
+  end
+
+  def point
+    case @number
+    when 'A'
+      1
+    when 'J', 'Q', 'K'
+      10
+    else
+      @number.to_i
+    end
+  end
+end
