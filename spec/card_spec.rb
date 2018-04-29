85788e5ece2ec2395a0cdc2bbe9e3ac10034dce4
Hidenori Miyoshi <miyohide@gmail.com>
2018-04-29 21:32:58 +0900

    Add Card#point spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -5,4 +5,14 @@ describe Card do
       expect(cards.count).to eq 52
     end
   end
+
+  describe "#point" do
+    it "return valid point" do
+      expect(Card.new(:heart, 'A').point).to eq 1
+      expect(Card.new(:heart, '2').point).to eq 2
+      expect(Card.new(:heart, 'J').point).to eq 10
+      expect(Card.new(:heart, 'Q').point).to eq 10
+      expect(Card.new(:heart, 'K').point).to eq 10
+    end
+  end
 end
