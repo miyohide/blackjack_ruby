e1f958a2d0b7c4f66095ed4d59bc30510abd3a7e
Hidenori Miyoshi <miyohide@gmail.com>
2018-04-29 18:45:52 +0900

    Fix bug.

--- lib/card.rb
+++ lib/card.rb
@@ -1,6 +1,6 @@
 class Card
   SUITS = %i(heart diamond club spade)
-  NUMBERS = %W(A 1 2 3 4 5 6 7 8 9 10 J Q K)
+  NUMBERS = %W(A 2 3 4 5 6 7 8 9 10 J Q K)
 
   def self.generate_stock
     SUITS.product(NUMBERS).map { |suit, number| self.new(suit, number) }.shuffle
