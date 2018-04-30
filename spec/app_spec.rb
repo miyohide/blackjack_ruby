5e8cef39f8f9cdfc60eb653241b2c0b9c812b772
Hidenori Miyoshi <miyohide@gmail.com>
2018-04-30 11:00:18 +0900

    Add dealer bust pattern.

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -79,5 +79,50 @@ describe App do
         end        
       end
     end
+
+    context "player win" do
+      context "dealer bust" do
+        let(:cards) do
+          [
+            Card.new(:spade, '3'), Card.new(:spade, 'J'),   # playerの手札
+            Card.new(:heart, '2'), Card.new(:heart, '3'),    # dealerの手札
+            Card.new(:spade, '2'),  # playerの手札
+            Card.new(:heart, '10'), Card.new(:heart, 'J')  # dealerの手札
+          ]
+        end
+
+        let(:answer) do
+          <<-ANSWER
+ブラックジャックゲームにようこそ
+あなたのカードはspadeの3です。
+あなたのカードはspadeのJです。
+ディーラーのカードはheartの2です
+ディーラーの2枚目のカードは分かりません
+現在のプレイヤーの得点は13です。
+カードを引きますか？
+あなたのカードはspadeの2です。
+現在のプレイヤーの得点は15です。
+カードを引きますか？
+現在のディーラーの得点は5です。
+ディーラーのカードはheartの10です
+現在のディーラーの得点は15です。
+ディーラーのカードはheartのJです
+25点でディーラーはbustしました。
+プレイヤーが勝ちました。
+さようなら。また遊んでね。
+          ANSWER
+        end
+
+        before do
+          allow(Card).to receive(:generate_stock).and_return(cards)
+        end
+
+        example do
+          app = App.new
+          allow(app).to receive(:answer_yes?).and_return(true, false)
+          expect { app.run }.to output(answer).to_stdout
+        end
+      end
+    end
   end
 end
