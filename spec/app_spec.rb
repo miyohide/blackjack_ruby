775505db0bc1eb897456bac2548471f70e16ab9a
Hidenori Miyoshi <miyohide@gmail.com>
2018-04-30 11:30:36 +0900

    add player win spec

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -123,6 +123,48 @@ describe App do
           expect { app.run }.to output(answer).to_stdout
         end
       end
+
+      context "player total > dealer total" do
+        let(:cards) do
+          [
+            Card.new(:spade, '3'), Card.new(:spade, 'J'),   # playerの手札
+            Card.new(:heart, '2'), Card.new(:heart, '3'),    # dealerの手札
+            Card.new(:spade, '7'),  # playerの手札
+            Card.new(:heart, '10'), Card.new(:heart, '4')  # dealerの手札
+          ]
+        end
+
+        let(:answer) do
+          <<-ANSWER
+ブラックジャックゲームにようこそ
+あなたのカードはspadeの3です。
+あなたのカードはspadeのJです。
+ディーラーのカードはheartの2です
+ディーラーの2枚目のカードは分かりません
+現在のプレイヤーの得点は13です。
+カードを引きますか？
+あなたのカードはspadeの7です。
+現在のプレイヤーの得点は20です。
+カードを引きますか？
+現在のディーラーの得点は5です。
+ディーラーのカードはheartの10です
+現在のディーラーの得点は15です。
+ディーラーのカードはheartの4です
+プレイヤーが勝ちました。
+さようなら。また遊んでね。
+          ANSWER
+        end
+
+        before do
+          allow(Card).to receive(:generate_stock).and_return(cards)
+        end
+
+        example do
+          app = App.new
+          allow(app).to receive(:answer_yes?).and_return(true, false)
+          expect { app.run }.to output(answer).to_stdout
+        end
+      end
     end
   end
 end
